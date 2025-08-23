d5396ee35d64a34d45fc390959de312e0a81d45a
“Thecnfor” <w5555wdnmd@gmail.com>
2025-08-23 22:41:42 +0800

    后台前端准备

---
+++ uiok/src/lib/store.ts
@@ -0,0 +1,25 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import appReducer from './features/app/appSlice';
+
+// 创建store配置
+export const store = configureStore({
+  reducer: {
+    app: appReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+  devTools: process.env.NODE_ENV !== 'production',
+});
+
+// 导出类型定义
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+// 创建类型化的hooks
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
